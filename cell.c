9a8570f3c71a2fd52bc48dc65db7453fc116619d
Alexandre-gng <gamepourjeux@gmail.com>
2023-10-25 10:51:53 +0200

    t_d_cell create_cell

--- cell.c
+++ cell.c
@@ -3,5 +3,9 @@
 #include "cell.h"
 
 t_d_cell* create_cell(int val, int level) {
-    t_d_cell* cell = (t_d_cell*) malloc(sizeof(val)*2);
+    t_d_cell* cell = (t_d_cell*) malloc(sizeof(t_d_cell));
+    cell->value = val;
+    cell->level = level;
+    cell->
+    return cell;
 }
