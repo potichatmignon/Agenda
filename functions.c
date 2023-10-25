279e1c91d9732e600dea2294ebc3a9c9769dac67
Alexandre-gng <gamepourjeux@gmail.com>
2023-10-25 11:18:46 +0200

    create_empty_list mmaj

--- functions.c
+++ functions.c
@@ -5,6 +5,8 @@
 t_d_list create_empty_list(int max_level) {
     t_d_list* empty_list = (t_d_list*) malloc(sizeof(t_d_list));
     empty_list->max_level = max_level;
-    empty_list->heads = NULL;
+    for (int i=0; i<max_level; i++) {
+        empty_list->heads[i] = NULL;
+    }
     return *empty_list;
 }
