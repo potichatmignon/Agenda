4f1e2d4f884b7a81eebaf52d13b736b6134da747
Alexandre-gng <gamepourjeux@gmail.com>
2023-10-25 11:04:50 +0200

    create_empty_list

--- cell.c
+++ cell.c
@@ -9,3 +9,10 @@ t_d_cell* create_cell(int val, int level) {
     cell->next = (t_d_cell**) malloc(level*sizeof(t_d_cell*));
     return cell;
 }
+
+t_d_list create_empty_list(int max_level) {
+    t_d_list* empty_list = (t_d_list*) malloc(sizeof(t_d_list));
+    empty_list->max_level = max_level;
+    empty_list->heads = NULL;
+    return *empty_list;
+}
