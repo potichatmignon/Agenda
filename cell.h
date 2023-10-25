c9c931867a7b48ef476e73d3a51ca88cfb36b16f
Arthur <84375397+potichatmignon@users.noreply.github.com>
2023-10-25 10:32:41 +0200

    added cell.c cell.h + struct in cell.h

---
+++ cell.h
@@ -0,0 +1,18 @@
+#ifndef AGENDA_CELL_H
+#define AGENDA_CELL_H
+
+typedef struct s_d_cell
+{
+    int value;
+    struct s_d_cell *next_0;
+    struct s_d_cell *next_1;
+} t_d_cell;
+
+typedef struct s_d_list
+{
+    t_d_cell *head_0;
+    t_d_cell *head_1;
+} t_d_list;
+
+
+#endif //AGENDA_CELL_H
